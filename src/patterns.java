70cfb323c352c01f4a4f30481f56566c2c9fe18d
Payal Rawat <payalrawat2611@gmail.com>
2026-09-02 13:17:25 +0530

    pattern probelem 6

--- src/patterns.java
+++ src/patterns.java
@@ -67,4 +67,17 @@ public class patterns {
         }
     }
 
+    // pattern 6 :
+    static void patternPrint(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print((j + 1) + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
