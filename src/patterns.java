01885137bbe93865aae7434c274388d9ec701f9e
Payal Rawat <payalrawat2611@gmail.com>
2026-09-02 13:08:00 +0530

    pattern probelm 4

--- src/patterns.java
+++ src/patterns.java
@@ -41,4 +41,17 @@ public class patterns {
         }
     }
 
+    // pattern 4
+    static void pattern4(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print((i + 1) + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
