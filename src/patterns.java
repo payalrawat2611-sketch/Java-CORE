fddf52ee55eba86b6fdda89cf7ec783de7514db9
Payal Rawat <payalrawat2611@gmail.com>
2026-09-02 13:09:02 +0530

    pattern probelm 5

--- src/patterns.java
+++ src/patterns.java
@@ -54,4 +54,17 @@ public class patterns {
         }
     }
 
+    // pattern 5 :
+    static void pattern5(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
