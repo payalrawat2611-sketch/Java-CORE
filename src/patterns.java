d9e2c262d7dafd307448e4735be1a2ddb50aa69e
Payal Rawat <payalrawat2611@gmail.com>
2026-09-06 00:15:44 +0530

    pattern problem 20

--- src/patterns.java
+++ src/patterns.java
@@ -334,4 +334,50 @@ public class patterns {
         }
     }
 
+    // pattern 20 :
+    static void pattern20(int n) {
+
+        // upper half
+        for (int i = 0; i < n; i++) {
+
+            // left stars
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // spaces
+            for (int j = 0; j < 2 * (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+
+            // right stars
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = 0; i < n; i++) {
+
+            // left stars
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("*");
+            }
+
+            // spaces
+            for (int j = 0; j < 2 * (i + 1); j++) {
+                System.out.print(" ");
+            }
+
+            // right stars
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
