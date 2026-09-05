42d221abbe21fcbf70499b4f659598cde86af6ee
Payal Rawat <payalrawat2611@gmail.com>
2026-09-05 23:40:10 +0530

    pattern 9

--- src/patterns.java
+++ src/patterns.java
@@ -118,6 +118,41 @@ public class patterns {
         }
     }
 
+    // pattern 9 :
+    static void pattern9(int n) {
+
+        // upper half
+        for (int i = 0; i < n; i++) {
+
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = 0; i < n; i++) {
+
+            // spaces
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 0; j < 2 * (n - i - 1) + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 
 
 }
