ad802a802ced22d26f1960a4f4cbb270d5bf27ae
Payal Rawat <payalrawat2611@gmail.com>
2026-09-05 23:41:54 +0530

    pattern problem 11

--- src/patterns.java
+++ src/patterns.java
@@ -177,5 +177,21 @@ public class patterns {
             System.out.println();
         }
     }
+    // pattern 11 :
+    static void pattern11(int n) {
 
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+
+            System.out.println();
+        }
+    }
 }
