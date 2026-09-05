3f95f520f415b6f8230ccc4bc7e18408c95c489e
Payal Rawat <payalrawat2611@gmail.com>
2026-09-06 00:10:04 +0530

    pattern problem 16

--- src/patterns.java
+++ src/patterns.java
@@ -237,4 +237,17 @@ public class patterns {
         }
     }
 
+    // pattern 16 :
+    static void pattern16(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) ('A' + i));
+            }
+
+            System.out.println();
+        }
+    }
+
 }
