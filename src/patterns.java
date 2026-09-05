ebd4a0669093eff8bc9f5f0a420cfd244fbc27c6
Payal Rawat <payalrawat2611@gmail.com>
2026-09-05 23:39:16 +0530

    pattern 8

--- src/patterns.java
+++ src/patterns.java
@@ -99,4 +99,25 @@ public class patterns {
         }
     }
 
+    // pattern 8 :
+    static void pattern8(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+
 }
