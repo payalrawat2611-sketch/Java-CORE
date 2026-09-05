334312a917d0ded2337e91b704f697985931d0f4
Payal Rawat <payalrawat2611@gmail.com>
2026-09-06 00:05:24 +0530

    pattern problem 15

--- src/patterns.java
+++ src/patterns.java
@@ -224,4 +224,17 @@ public class patterns {
         }
     }
 
+    // pattern 15 :
+    static void pattern15(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print((char) ('A' + j));
+            }
+
+            System.out.println();
+        }
+    }
+
 }
