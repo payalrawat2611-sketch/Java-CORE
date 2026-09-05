8ffb5cbf4c18920c9d1478265c11751dbc74b273
Payal Rawat <payalrawat2611@gmail.com>
2026-09-05 23:44:55 +0530

    pattern problem 13

--- src/patterns.java
+++ src/patterns.java
@@ -218,4 +218,28 @@ public class patterns {
             System.out.println();
         }
     }
+
+    // pattern 12 :
+    static void pattern12(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            // left numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // right numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
 }
