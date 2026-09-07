ecdb579e84da81d49638ee52a45fff69857cf8c2
Payal Rawat <payalrawat2611@gmail.com>
2026-09-07 23:36:17 +0530

    pattern [roblem 21

--- src/patterns.java
+++ src/patterns.java
@@ -379,5 +379,22 @@ public class patterns {
             System.out.println();
         }
     }
+    // pattern 21 :
+    static void pattern21(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
 
 }
