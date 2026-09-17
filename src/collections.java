e683974fe3ec90cb85e94001d6b86488d1906405
Payal Rawat <payalrawat2611@gmail.com>
2026-09-17 23:57:24 +0530

    added ArrayList basics

---
+++ src/collections.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+public class collections {
+
+    public static void main(String[] args) {
+
+            // Creating an ArrayList
+        ArrayList<String> names = new ArrayList<>();
+
+            // Adding elements
+        names.add("Payal");
+        names.add("Rahul");
+        names.add("Aman");
+        names.add("Neha");
+        System.out.println("Original ArrayList: " + names);
+
+            // Accessing an element
+        System.out.println("First name: " + names.get(0));
+
+            // Updating an element
+        names.set(1, "Riya");
+        System.out.println("After updating: " + names);
+
+            // Removing an element
+        names.remove(2);
+        System.out.println("After removing: " + names);
+
+            // Finding size
+        System.out.println("Size: " + names.size());
+
+            // Checking whether an element exists
+        System.out.println("Contains Payal: " + names.contains("Payal"));
+
+            // Traversing ArrayList
+        System.out.println("All names:");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
