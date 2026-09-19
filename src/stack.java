75457f064ba41d7118454f95914ac446a2a388c4
Payal Rawat <payalrawat2611@gmail.com>
2026-09-20 00:02:30 +0530

    Stacks

--- src/stack.java
+++ src/stack.java
@@ -7,12 +7,7 @@ public class StackDemo {
         // Creating a Stack of Integer type
         Stack<Integer> stack = new Stack<>();
 
-
-        // --------------------------------------------------
-        // 1. PUSH
-        // --------------------------------------------------
-
-        // push() adds an element to the top of Stack
+        // 1. PUSH --->  push() adds an element to the top of Stack
         stack.push(10);
         stack.push(20);
         stack.push(30);
@@ -20,67 +15,38 @@ public class StackDemo {
         System.out.println("Stack: " + stack);
 
 
-        // --------------------------------------------------
-        // 2. PEEK
-        // --------------------------------------------------
-
-        // peek() returns the top element
+        // 2. PEEK ---> peek() returns the top element
         // It does NOT remove the element
         System.out.println("Top element: " + stack.peek());
 
         System.out.println("Stack after peek: " + stack);
 
 
-        // --------------------------------------------------
-        // 3. POP
-        // --------------------------------------------------
-
-        // pop() removes and returns the top element
+        // 3. POP ---->  pop() removes and returns the top element
         int removed = stack.pop();
 
         System.out.println("Removed element: " + removed);
         System.out.println("Stack after pop: " + stack);
 
-
-        // --------------------------------------------------
         // 4. PUSH ANOTHER ELEMENT
-        // --------------------------------------------------
 
         stack.push(40);
 
         System.out.println("After pushing 40: " + stack);
 
 
-        // --------------------------------------------------
-        // 5. SEARCH
-        // --------------------------------------------------
-
-        // search() searches an element from the TOP
-        // Position starts from 1
+        // 5. SEARCH ----> search() searches an element from the TOP (Position starts from 1)
         System.out.println("Position of 20: " + stack.search(20));
 
 
-        // --------------------------------------------------
-        // 6. CHECK EMPTY
-        // --------------------------------------------------
-
-        // empty() returns true if Stack contains no elements
+        // 6. CHECK EMPTY --->  empty() returns true if Stack contains no elements
         System.out.println("Is Stack empty: " + stack.empty());
 
-
-        // --------------------------------------------------
-        // 7. SIZE
-        // --------------------------------------------------
-
-        // size() returns number of elements
+        // 7. SIZE --->  size() returns number of elements
         System.out.println("Stack size: " + stack.size());
 
 
-        // --------------------------------------------------
-        // 8. ACCESS ELEMENT USING INDEX
-        // --------------------------------------------------
-
-        // Since Stack extends Vector, we can also use get()
+        // 8. ACCESS ELEMENT USING INDEX ---> Since Stack extends Vector, we can also use get()
         System.out.println("Element at index 0: " + stack.get(0));
 
 
